429da029cfa6ae34ff888289df0e3e1d4a657077
CODEisArt <105768058+CODEisArrrt@users.noreply.github.com>
2022-08-21 04:18:23 +0800

    Update run-validator-node.sh

--- run-validator-node.sh
+++ run-validator-node.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-wget https://github.com/CODEisArrrt/aptos-ait3/releases/download/v0.1.0/aptos.tar 
+
+if [ ! -f "aptos.tar" ]; then
+    wget https://github.com/CODEisArrrt/aptos-ait3/releases/download/v0.1.0/aptos.tar 
+fi
+
+rm -rf aptos
+
+echo "解压数据中..."
+
 tar -xzvf aptos.tar
+
 cd aptos 
+
 sudo ./run-testnet.sh $*
